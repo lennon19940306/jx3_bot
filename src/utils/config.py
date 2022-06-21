20eaf10342a3f09a8dc5cc3c1cf991866ca09c8e
lennon <1136160757@qq.com>
2022-06-21 17:43:56 +0800

    1、添加新的配置文件，允许secret.yml覆盖config.yml下的配置（仅支持一级直接覆盖）

--- src/utils/config.py
+++ src/utils/config.py
@@ -12,22 +12,33 @@ def config_init():
     初始化读取配置文件，需要在所有项目之前启动
     '''
     global config
+    configf: Optional[dict[str, dict[str, Union[str, int, bool]]]] = None
+    config2: Optional[dict[str, dict[str, Union[str, int, bool]]]] = None
+    # config3: Optional[dict[str, dict[str, Union[str, int, bool]]]] = Optional[dict[str, dict[str, Union[str, int, bool]]]]()
     with open('config.yml', 'r', encoding='utf-8') as f:
         cfg = f.read()
-        config = yaml.load(cfg, Loader=yaml.FullLoader)
+        configf = yaml.load(cfg, Loader=yaml.FullLoader)
+    if os.path.exists('secret.yml'):
+        with open('secret.yml', 'r', encoding='utf-8') as s:
+            scfg = s.read()
+            config2 = yaml.load(scfg, Loader=yaml.FullLoader)
+            for x in config2.keys():
+                configf[x] = config2.get(x)
 
+    config = configf
+    # print(config3)
     # 判断项目目录是否存在
     path = config.get('path')
     data = path.get('data')
     if not os.path.exists(data):
         os.makedirs(data)
     log = path.get('log')
-    info = log+'info'
+    info = log + 'info'
     if not os.path.exists(info):
         os.makedirs(info)
-    debug = log+'debug'
+    debug = log + 'debug'
     if not os.path.exists(debug):
         os.makedirs(debug)
-    error = log+'error'
+    error = log + 'error'
     if not os.path.exists(error):
         os.makedirs(error)
